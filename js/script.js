6127cb3007cfb417bb7cfb1ec2a1fe0cc8aa602d
Daria <139646443+DariaSS3003@users.noreply.github.com>
2026-02-18 17:51:20 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var swiper = new Swiper(".about__swiper-pagination", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: "auto",
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swiper2 = new Swiper(".about__swiper-main", {
+  loop: true,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2000,
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+const projectItems = document.querySelectorAll(".projects__item");
+
+projectItems[0].classList.add("item-active");
+
+projectItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    projectItems.forEach((e) => {
+      e.classList.remove("item-active");
+    });
+    this.classList.add("item-active");
+  });
+
+  item.addEventListener("mouseenter", function () {
+    if (!this.classList.contains("item-active")) {
+      this.classList.add("item-hover");
+    }
+  });
+  item.addEventListener("mouseleave", function () {
+    this.classList.remove("item-hover");
+  });
+});
